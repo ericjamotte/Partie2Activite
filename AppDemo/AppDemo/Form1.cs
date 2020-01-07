0a99789666aa802d1014bf0767713699f0cfe199
ericjamotte <psr09636@students.ephec.be>
2020-01-07 14:05:41 +0100

    Ajout d'une fonction au bouton push me qui affiche un message sur clic

--- AppDemo/AppDemo/Form1.cs
+++ AppDemo/AppDemo/Form1.cs
@@ -16,5 +16,10 @@ namespace AppDemo
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks for pushing button");
+        }
     }
 }
